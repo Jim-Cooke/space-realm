c7242980c8367a5517adfa8955f8a1ec07a2900e
Jim Cooke <jimcoo2003@yahoo.ca>
2020-12-06 17:37:18 -0500

    Add files via upload

---
+++ star_empire_obj2.py
@@ -0,0 +1,77 @@
+# star empire program
+# import library with random function
+# 
+import random
+import time
+import math
+
+player = ["XXX"]
+turns = []
+
+#my random function
+def random_1 ():
+    r = random.random()
+    t = time.time()
+    # print(r, " ", t)
+    rs = r + t
+    # print(rs)
+    rs = rs - math.floor(rs)
+    return rs
+
+# generate worlds
+# prompt for user imput
+nop = int(input("How many players?"))
+#  print(nop)
+wpp = int(input("How many worlds per player?"))
+
+# enter player names
+for n in range(1, nop + 1):
+    print("Three-letter name of player ", n, "?")
+    # name = input()
+    player.append(input())
+    turns.append(0)
+    # print(player[n])
+    
+#  find size of cube containing worlds 
+cube_side = (nop*wpp*27)**(1./3.)
+
+
+planet = [1]
+for n in range (1, nop*wpp+1):
+    planet.append(n)
+
+class worlds :pass
+for n in range (1, nop*wpp+1):
+    planet[n] = worlds()
+    if n <= nop:
+        planet[n].owner = player[n]
+    else:
+        planet[n].owner = "XXX"
+    planet[n].locx = int(random_1()*cube_side - cube_side/2)
+    planet[n].locy = int(random_1()*cube_side - cube_side/2)
+    planet[n].locz = int(random_1()*cube_side - cube_side/2)
+    planet[n].res_prod = int(random_1()*2 + 1)
+    planet[n].res = int(random_1()*2 + 1)
+    if planet[n].owner != "XXX":
+        planet[n].ap = int(random_1()*2 + 1)
+        planet[n].pop = int(random_1()*2 + 1)
+        planet[n].ind = int(random_1()*2 + 1)
+        planet[n].ships = 5
+    else:
+        planet[n].ap = 0
+        planet[n].pop = 0
+        planet[n].ind = 0
+        planet[n].ships = 0
+    planet[n].ind_prod = min(planet[n].res, planet[n].pop, planet[n].ind)
+
+
+    
+#display worlds
+    
+print("Wld  Ownr  X   Y   Z    Rp  Res  A+P  Pop  Ind  IP  Sh")
+for n in range (1, nop*wpp+1):
+    loc_line = '%4s, %4s, %4s' % (planet[n].locx, planet[n].locy, planet[n].locz)
+    print(n, "  ", planet[n].owner, " ",loc_line, " ", planet[n].res_prod, " ", planet[n].res, " ", planet[n].ap,
+    " ", planet[n].pop, " ", planet[n].ind, " ", planet[n].ind_prod, " ", planet[n].ships) 
+
+#          
