222fdf21a157c8dc29790ea50366ce86881f3f7d
Jim Cooke <jimcoo2003@yahoo.ca>
2020-12-26 16:41:05 -0500

    Add files via upload

--- star_empire_obj2.py
+++ star_empire_obj2.py
@@ -7,6 +7,8 @@ import math
 
 player = ["XXX"]
 turns = []
+current_turn = 1
+current_player = "XXX"
 
 #my random function
 def random_1 ():
@@ -17,6 +19,7 @@ def random_1 ():
     # print(rs)
     rs = rs - math.floor(rs)
     return rs
+
 
 # generate worlds
 # prompt for user imput
@@ -64,14 +67,47 @@ for n in range (1, nop*wpp+1):
         planet[n].ships = 0
     planet[n].ind_prod = min(planet[n].res, planet[n].pop, planet[n].ind)
 
+#display worlds function
+def display_wrld (worlds, num):
+    print(" ")
+    print("Wld  Ownr     X    Y    Z   Rp  Res A+P Pop Ind IP Sh")
+    for n in range (1, num+1):
+        pl_loc_line = '%4s %4s %4s' % (planet[n].locx, planet[n].locy, planet[n].locz)
+        print(n, "  ", planet[n].owner, " ",pl_loc_line, " ", planet[n].res_prod, " ", planet[n].res, " ", planet[n].ap,
+        " ", planet[n].pop, " ", planet[n].ind, " ", planet[n].ind_prod, " ", planet[n].ships) 
 
-    
-#display worlds
-    
-print("Wld  Ownr  X   Y   Z    Rp  Res  A+P  Pop  Ind  IP  Sh")
-for n in range (1, nop*wpp+1):
-    loc_line = '%4s, %4s, %4s' % (planet[n].locx, planet[n].locy, planet[n].locz)
-    print(n, "  ", planet[n].owner, " ",loc_line, " ", planet[n].res_prod, " ", planet[n].res, " ", planet[n].ap,
-    " ", planet[n].pop, " ", planet[n].ind, " ", planet[n].ind_prod, " ", planet[n].ships) 
+#call display worlds function
+display_wrld(worlds, nop*wpp)
 
-#          
+#fleets
+fleet = [1]
+
+#number of fleets    
+nof = 1
+
+class armada :pass
+for n in range (0, nof):
+    fleet[n] = armada()
+    fleet[n].owner = "XXX"
+    fleet[n].locx = 0
+    fleet[n].locy = 0
+    fleet[n].locz = 0
+    fleet[n].dest_wrld = 0
+    fleet[n].dest_x = 0
+    fleet[n].dest_y = 0
+    fleet[n].dest_z = 0
+    fleet[n].ships = 0
+    fleet[n].cargo = 0
+    fleet[n].crg_type = "_"
+
+#display fleets function
+def display_flt (armada, nof):
+    print(" ")
+    print("Flt Ownr     X    Y    Z   Sh  Cargo")
+    for n in range (0, nof):
+        ft_loc_line = '%4s %4s %4s' % (fleet[n].locx, fleet[n].locy, fleet[n].locz)
+        print(n, " ", fleet[n].owner, " ", ft_loc_line, " ", fleet[n].ships, " ", fleet[n].cargo, " ", fleet[n].crg_type)
+
+#call display fleets function
+display_flt(armada, nof)
+    
