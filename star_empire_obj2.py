baeec79932f1798a81f1250029f0b613f7a38969
Jim Cooke <jimcoo2003@yahoo.ca>
2022-12-17 16:58:40 -0500

    Add files via upload

--- star_empire_obj2.py
+++ star_empire_obj2.py
@@ -26,7 +26,6 @@ def random_1():
 nop = int(input("How many players?"))
 #  print(nop)
 wpp = int(input("How many worlds per player?"))
-no_worlds = nop*wpp
 
 # enter player names
 for n in range(1, nop + 1):
@@ -41,11 +40,11 @@ cube_side = (nop*wpp*27)**(1./3.)
 
 
 planet = [1]
-for n in range (1, no_worlds+1):
+for n in range (1, nop*wpp+1):
     planet.append(n)
 
 class worlds :pass
-for n in range (1, no_worlds+1):
+for n in range (1, nop*wpp+1):
     planet[n] = worlds()
     if n <= nop:
         planet[n].owner = player[n]
@@ -91,7 +90,7 @@ fleet[0].crg_type = "_"
 #crg_type - ap, pop, ind
 fleet[0].status = "null"
 # status - null, inflight, still
-# fleet[0].arr = 0
+fleet[0].arr = 0
 # arr - arrival time
 
 
@@ -125,6 +124,7 @@ def display_flt(armada, worlds, nof, num, df_player):
 
             
 #display worlds function
+# you can see a world if it is in the same location as one of your worlds or fleets
 def display_wrld(armada, worlds, num, dw_player, nof):
     print(" ")
     print("Wld  Ownr     X    Y    Z   Rp  Res A+P Pop Ind IP Sh")
@@ -142,7 +142,6 @@ def display_wrld(armada, worlds, num, dw_player, nof):
             print(n, "  ", planet[n].owner, " ",pl_loc_line, pr_kn_wo_prd, pr_kn_wo_ind)
         else:
             print(n, "  ", planet[n].owner, " ",pl_loc_line)
-# also you can see a world if is in the same location as one of your worlds or fleets
 
 #build on worlds function
 def build(worlds, num, player):
@@ -168,36 +167,40 @@ def build(worlds, num, player):
             if th == 3:
                 planet[p].ap = planet[p].ap + n
     else:
-        print("You don't own that planet.")
+        print("You don't own that planet")
 
-#events
-event = [1]
-for n in range (1, no_worlds+1):
-    event.append(n)
-#number of events
-noe = nop*wpp
-
-#events - planet production "prod", fleet arrives "arr"
-#time = -1 means unused event
-# planet or fleet number
-class turns :pass
-#seed event table with planet production events for each world
-for n in range (1, noe+1):
-    event[n] = turns()
-    event[n].time = 1 + 2*random_1()
-    event[n].type = "prod"
-    event[n].number = n
+        
+def travel_time(orig_x, orig_y, orig_z, dest_x, dest_y, dest_z)
+    dist = math.sqrt((orig_x-dest_x)**2 + (orig_y-dest_y)**2 + (orig_z-dest_z)**2)
+    return 2 * math.sqrt(dist)
 
 
 #send_fleet function
-def send_fleet(worlds, planets, player, armada, nof, no_worlds):
+def send_fleet(worlds, planets, guy, armada, nof, current_turn):
     # fl is fleet number and d_pl is destination planet s_pl start planet
     # send existing fleet or create new one?
     ch = "x"
     while ch != "e" and ch != "n":
         ch = input("Enter e for existing fleet or n for new fleet")
+    #existing fleet
+    if ch == "e":   
+    # make sure fleet exists and is owned by player
+    # which fleet?
+    if own_fl == "yes":
+        fleet_ok = "no"    
+        while fleet_ok != "yes":
+            fl = int(input("What fleet to use? Enter -1 to not select any fleet"))
+            fleet_ok = "yes"
+            if fl >= nof or fl < 0:
+                fleet_ok = "no"
+            if fl == -1:
+                return
+            if fleet[fl].owner != guy:
+                fleet_ok = "no"
+    # if fleet not selected drop out of function
+    #new fleet
     # find or create fleet table entry
-    if ch == "n":  #new fleet
+    if ch == "n":  
         fl = 0
         while fl <= nof:
             if fl == nof:
@@ -209,34 +212,46 @@ def send_fleet(worlds, planets, player, armada, nof, no_worlds):
                 break
             fl = fl +1
         # create fleet
-        fleet[fl].owner = player
-        fleet[fl].status = "inflight"
+        fleet[fl].owner = guy
         s_pl = int(input("what planet to start from?"))
-        while planet[s_pl].owner != player:
-            s_pl = int(input("what planet to start from?"))
+        while planet[s_pl].owner != guy:
+            s_pl = int(input("what planet to start from? Enter -1 to not select a planet"))
+            if s_pl == -1:
+                return
+    #if planet not selected drop out of function
+        #fleet location is location of starting planet
+        fleet[fl].locx = planet[s_pl].locx
+        fleet[fl].locy = planet[s_pl].locy
+        fleet[fl].locz = planet[s_pl].locz
         # ships
         while True:
             fleet[fl].ships = int(input("How many ships?"))
             if fleet[fl].ships > 0 and fleet[fl].ships <= planet[s_pl].ships:
                 break
         planet[s_pl].ships = planet[s_pl].ships - fleet[fl].ships
+        if planet[a_pl].ships == 0:
+            planet[a_pl].owner = "XXX"
+            print("You no longer own planet ", a_pl)
         #cargo
         cargo_ok = "no"
         while cargo_ok == "no":
+            # cargo type
+            fleet[fl].crg_type = "_"
+            while fleet[fl].crg_type != "ap" and fleet[fl].crg_type != "pop" and fleet[fl].crg_type != "ind":
+                fleet[fl].crg_type = input("Enter cargo type: ap, pop, ind")
             #cargo amt
             fleet[fl].cargo = int(input("Amount of cargo?"))
             if fleet[fl].cargo == 0:
                 break
             if fleet[fl].cargo < 0:
                 cargo_ok = "no"
+                print("Incorrect amount")
                 continue
-            # cargo type
-            fleet[fl].crg_type = "_"
-            while fleet[fl].crg_type != "ap" and fleet[fl].crg_type != "pop" and fleet[fl].crg_type != "ind":
-                fleet[fl].crg_type = input("Enter cargo type: ap, pop, ind")
+            # enough?                
             if fleet[fl].crg_type == "ap":
                 if fleet[fl].cargo <= planet[s_pl].ap:
                    planet[s_pl].ap =  planet[s_pl].ap - fleet[fl].cargo
+                   cargo_ok = "yes"
                    break
                 else:
                     print("Not enough on the planet")
@@ -245,6 +260,7 @@ def send_fleet(worlds, planets, player, armada, nof, no_worlds):
             if fleet[fl].crg_type == "pop":
                 if fleet[fl].cargo <= planet[s_pl].pop:
                    planet[s_pl].pop =  planet[s_pl].pop - fleet[fl].cargo
+                   cargo_ok = "yes"
                    break
                 else:
                     print("Not enough on the planet")
@@ -253,22 +269,13 @@ def send_fleet(worlds, planets, player, armada, nof, no_worlds):
             if fleet[fl].crg_type == "ind":                     
                 if fleet[fl].cargo <= planet[s_pl].ind:
                    planet[s_pl].ind =  planet[s_pl].ind - fleet[fl].cargo
+                   cargo_ok = "yes"
                    break
                 else:
                     print("Not enough on the planet")
                     cargo_ok = "no"
                     continue
-    # continue building the fleet entry
-    if ch == "e":   #existing fleet
-        # make sure fleet exists and is owned by player
-        fleet_ok = "no"
-        while fleet_ok != "yes":
-            fl = int(input("What fleet to use?"))
-            fleet_ok = "yes"
-            if fl >= nof or fl < 0:
-                fleet_ok = "no"
-            if fleet[fl].owner != player:
-                fleet_ok = "no"
+    #destination
     # planet or location?
     dest = "x"
     while dest != "p" and dest != "l":
@@ -276,42 +283,39 @@ def send_fleet(worlds, planets, player, armada, nof, no_worlds):
     if dest == "p":
         #create entry for planet
         d_pl = -1
-        while d_pl < 1 or d_pl > no_worlds:
+        while d_pl < 1 or d_pl > nop*wpp:
             d_pl = int(input("Enter destination planet"))
-        fleet[fl].dest_wrld = d_pl 
+        fleet[fl].dest_wrld = d_pl
+        fleet[fl].dest_x = planet[d_pl].locx
+        fleet[fl].dest_y = planet[d_pl].locy
+        fleet[fl].dest_z = planet[d_pl].locz
     if dest == "l":
     #create entry for location
         fleet[fl].dest_x = int(input("Enter destination x co-ordinate"))
         fleet[fl].dest_y = int(input("Enter destination y co-ordinate"))
-        fleet[fl].dest_z = int(input("Enter destination z co-ordinate"))
-    # find arrival time
-    # find current location
-    # if new fleet
-    if ch == "n":
-        c_locx = planet[s_pl].locx
-        c_locy = planet[s_pl].locy
-        c_locz = planet[s_pl].locz
-    # if existing fleet
-    if ch == "e":
-        c_locx = fleet[fl].locx
-        c_locy = fleet[fl].locy
-        c_locz = fleet[fl].locz
-    # find destination location
-    # if dest is planet
-    if dest == "p":
-        d_locx = planet[d_pl].locx
-        d_locy = planet[d_pl].locy
-        d_locz = planet[d_pl].locz
-    # if dest is location
-    if dest == "l":
-        d_locx = fleet[fl].dest_x
-        d_locy = fleet[fl].dest_y
-        d_locz = fleet[fl].dest_z
-    # calculate arrival time
-    dist = math.sqrt((c_locx - d_locx)**2 + (c_locy - d_locy)**2 + (c_locz - d_locz)**2)
-    arr_t = 2*math.sqrt(dist)
-    # put arrival in event table
-    # find empty event record, or create new one, enter fleet arrival time
+        fleet[fl].dest_z = int(input("Enter destination z co-ordinate"))     
+    #calculate arrival time
+    tt = travel_time(fleet[fl].locx, fleet[fl].locy, fleet[fl].locz, fleet[fl].dest_x, fleet[fl].dest_y, fleet[fl].dest_z)
+    fleet[fl].arr = current_turn + tt
+
+#events
+event = [1]
+for n in range (1, nop*wpp+1):
+    event.append(n)
+
+#number of events
+noe = nop*wpp
+
+#events - planet production "prod", fleet arrives "arr"
+#time = -1 means unused event
+# planet or fleet number
+class turns :pass
+#seed event table with planet production events
+for n in range (1, noe+1):
+    event[n] = turns()
+    event[n].time = 1 + 2*random_1()
+    event[n].type = "prod"
+    event[n].number = n
     
 #structure of game
 #choose player, player takes turn
@@ -364,10 +368,10 @@ while end_game != "yes":
             if ch2 == 4:
                 build(worlds, nop*wpp, player[t_player])
             if ch2 == 5:
-                send_fleet(worlds, nop*wpp, player[t_player], armada, nof, no_worlds)
+                send_fleet(worlds, nop*wpp, player[t_player], armada, nof, current_turn)
             #other choices call functions
         #check event table, process events
-        #still doing send fleets - about to put new fleet arrival into event table     
+        #still doing send fleets - building the new fleet entry        
         
         
 
